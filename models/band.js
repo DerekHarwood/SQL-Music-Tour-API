a4074f0ca852c07e913be0f7521a00773be1b6ca
DerekHarwood <101199163+DerekHarwood@users.noreply.github.com>
2022-09-25 16:25:07 -0400

    Update band.js

--- models/band.js
+++ models/band.js
@@ -3,45 +3,59 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Band extends Model {
+  class band extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate({meet_greet, set_time }) {
       // define association here
+      // band id, start time, end time
+      //band.belongsToMany(models.event, {foreignKey: 'band_id'});
+      band.hasMany(meet_greet, {
+        foreignKey: 'band_id',
+        as: 'meet_greets',
+      });
+      band.hasMany(set_time, {
+        foreignKey: 'band_id',
+        as: 'set_times',
+      });
+
     }
   }
-  Band.init({
+  band.init({
     band_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
+      allowNull: false
     },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
+    band_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-    genre: {
-        type: DataTypes.TEXT,
-        allowNull: false
+    band_members: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    band_genre: {
+      type: DataTypes.TEXT,
+      allowNull: false,
     },
     available_start_time: {
-        type: DataTypes.DATE,
-        allowNull: false
+      type: DataTypes.DATE,
+      allowNull: false,
     },
     end_time: {
-        type: DataTypes.DATE,
-        allowNull: false
-    }
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
   }, {
     sequelize,
-    modelName: 'Band',
+    modelName: 'band',
     tableName: 'bands',
-    timestamps: false
-  })
+    timestamps: false,
+  });
+  return band;
 };
-
-// EXPORT
-module.exports = Band
